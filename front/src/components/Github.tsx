ede9cb46158072215db56e768cbcd6e510dff35b
ayaka kawabe <kawabe.ayaka@icloud.com>
2024-02-19 16:46:51 +0900

    add fetch repo data func

--- front/src/components/Github.tsx
+++ front/src/components/Github.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { GithubRepoData } from "./api/Github";
 
 const GithubRepo:React.FC=()=>{
+    const repo=GithubRepoData("chatgpt-line-bot-for-experiment")
+    console.log(repo)
     return(
         <div>
             <h1>Github</h1>
+            <p>{repo.name}</p>
         </div>
     )
 };

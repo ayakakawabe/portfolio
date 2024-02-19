9bfbb9c513799568aa4f1ae2596a09ac43b2b8f2
ayaka kawabe <kawabe.ayaka@icloud.com>
2024-02-19 23:59:37 +0900

    display github repo data

--- front/src/components/Github.tsx
+++ front/src/components/Github.tsx
@@ -1,14 +1,53 @@
-import React from "react";
-import { GithubRepoData } from "./api/Github";
+import React, { useEffect, useState } from "react";
+
+interface RepoType{
+    name:string,
+    description:string,
+    language:Array<String>,
+    url:string
+}
+
+const fetchAllRepos= async():Promise<[]>=>{
+    const response= await fetch("https://api.github.com/users/ayakakawabe/repos");
+    if(!response.ok){
+        throw new Error(`HTTP error. Status:${response.status}`);
+    }
+    const data=response.json()
+    return data;
+};
 
 const GithubRepo:React.FC=()=>{
-    const repo=GithubRepoData("chatgpt-line-bot-for-experiment")
-    console.log(repo)
+
+    const repoList:Array<string>=["chatgpt-line-bot-for-experiment","portfolio"];
+    
+    const [repos,setRepos]=useState<Array<RepoType>>([]);
+    useEffect(()=>{
+        (async()=>{
+            const allRepos=await fetchAllRepos();
+            allRepos.forEach((repo)=>{
+                    if(repoList.includes(repo.name)){
+                        console.log(repo)
+                        setRepos(repos=>[...repos,{name:repo.name,language:[repo.language],description:repo.description,url:repo.html_url}]);
+                    }
+                });
+        })();
+    },[]);
     return(
         <div>
             <h1>Github</h1>
-            <p>{repo.name}</p>
+                <div>
+                    {repos && 
+                    repos.map((repo,index)=>{
+                        return (
+                            <div key={index}>
+                                <p>{repo.name}</p>
+                                <p>{repo.description}</p>
+                                <p>{repo.language}</p>
+                            </div>
+                            )
+                    })}
+                </div>
         </div>
     )
-};
+}
 export default GithubRepo;

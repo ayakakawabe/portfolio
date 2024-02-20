e5d959e2f8a21d21aad0b428633d45a2bf2e265b
ayaka kawabe <kawabe.ayaka@icloud.com>
2024-02-20 12:38:45 +0900

    add github account info

--- front/src/components/Github.tsx
+++ front/src/components/Github.tsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
 
+interface AccountInfoType{
+    name:string,
+    avatarUrl:string,
+    repos:number,
+    following:number,
+    followers:number
+}
+
 interface RepoType{
     name:string,
     description:string,
@@ -7,23 +15,39 @@ interface RepoType{
     url:string
 }
 
-const fetchAllRepos= async():Promise<Array<object>>=>{
-    const response= await fetch("https://api.github.com/users/ayakakawabe/repos");
+const acconutName:string="ayakakawabe";
+const repoList:Array<string>=["chatgpt-line-bot-for-experiment","portfolio"];
+
+const fetchAccountInfo= async(acconutName:string):Promise<object>=>{
+    const response= await fetch(`https://api.github.com/users/${acconutName}`);
     if(!response.ok){
-        throw new Error(`HTTP error. Status:${response.status}`);
+        throw new Error(`HTTP error (fetch account info ). Status:${response.status}`)
+    }
+    const data=response.json();
+    return data;
+};
+
+const fetchAllRepos= async(acconutName:string):Promise<Array<object>>=>{
+    const response= await fetch(`https://api.github.com/users/${acconutName}/repos`);
+    if(!response.ok){
+        throw new Error(`HTTP error (fetch repos ). Status:${response.status}`);
     }
     const data=response.json()
     return data;
 };
 
 const GithubRepo:React.FC=()=>{
-
-    const repoList:Array<string>=["chatgpt-line-bot-for-experiment","portfolio"];
-    
+    const [accountInfo,setAccountInfo]=useState<AccountInfoType>();    
     const [repos,setRepos]=useState<Array<RepoType>>([]);
+
     useEffect(()=>{
         (async()=>{
-            const allRepos=await fetchAllRepos();
+            const allAccountInfo= await fetchAccountInfo(acconutName);
+            setAccountInfo({name:acconutName,avatarUrl:allAccountInfo.avatar_url,repos:allAccountInfo.public_repos,following:allAccountInfo.following,followers:allAccountInfo.followers});
+        })();
+
+        (async()=>{
+            const allRepos=await fetchAllRepos(acconutName);
             allRepos.forEach((repo)=>{
                 if(repoList.includes(repo.name)){
                     console.log(repo)
@@ -32,21 +56,29 @@ const GithubRepo:React.FC=()=>{
             });
         })();
     },[]);
+
     return(
         <div>
             <h1>Github</h1>
-                <div>
-                    {repos && 
-                    repos.map((repo,index)=>{
-                        return (
-                            <div key={index}>
-                                <p>{repo.name}</p>
-                                <p>{repo.description}</p>
-                                <p>{repo.language}</p>
-                            </div>
-                            )
-                    })}
-                </div>
+            <div>
+                <img src={accountInfo?.avatarUrl}></img>
+                <p>{accountInfo?.name}</p>
+                <p>{accountInfo?.repos}</p>
+                <p>{accountInfo?.following}</p>
+                <p>{accountInfo?.followers}</p>                
+            </div>
+            <div>
+                {repos && 
+                repos.map((repo,index)=>{
+                    return (
+                        <div key={index}>
+                            <p>{repo.name}</p>
+                            <p>{repo.description}</p>
+                            <p>{repo.language}</p>
+                        </div>
+                        )
+                })}
+            </div>
         </div>
     )
 }
